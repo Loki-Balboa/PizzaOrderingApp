b11074edbecd8a73c151ce941d4dce5416a66eef
Piotr Lubszczyk <plubszczyk@gmail.com>
2019-05-15 17:43:43 +0200

    Added list of ingredients to CreatePizzaWindow

--- pizzeria/CreatePizzaWindow.xaml.cs
+++ pizzeria/CreatePizzaWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Pizzeria
 {
@@ -20,9 +8,12 @@ namespace Pizzeria
     /// </summary>
     public partial class CreatePizzaWindow : Window
     {
+        public Menu menu = new Menu();
+
         public CreatePizzaWindow()
         {
             InitializeComponent();
+            IngredientsList.ItemsSource = menu.Ingredients;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
